3f1cba889b965f6bb6ded42204888b2f8e5bedf5
ItsJonQ <itsjonq@gmail.com>
2018-03-07 14:01:50 -0500

    Add Sixty

---
+++ configs/schemes/sixty.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'Sixty',
+
+  baseDark: '282c34',
+  baseLight: 'eff1f5',
+
+  baseColors: {
+    red: 'c678dd',
+    orange: 'c678dd',
+    yellow: '61afef',
+    green: '98c379',
+    cyan: '56b6c2',
+    blue: '61afef',
+    magenta: 'c678dd',
+    darkRed: 'c678dd'
+  }
+}
