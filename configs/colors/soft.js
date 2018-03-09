c01e95dc52e417c8a640d7f582c948e0ca1e8e6c
ItsJonQ <itsjonq@gmail.com>
2018-03-09 16:27:32 -0500

    Update seventy

--- configs/colors/soft.js
+++ configs/colors/soft.js
@@ -1,10 +1,10 @@
 module.exports = {
   red: 'e06c75',
-  orange: 'c678dd',
+  orange: 'EBB790',
   yellow: 'f0c674',
   green: '8dc891',
   cyan: '56b6c2',
   blue: '61afef',
-  magenta: 'c5a5c5',
+  magenta: 'c68cdd',
   darkRed: 'e06c75',
 }
