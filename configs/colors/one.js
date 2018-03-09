a29b48b6d0cecd2c8e286c1b10240dac464604a0
ItsJonQ <itsjonq@gmail.com>
2018-03-09 15:50:01 -0500

    Update

---
+++ configs/colors/one.js
@@ -0,0 +1,10 @@
+module.exports = {
+  red: 'e06c75',
+  orange: 'd08770',
+  yellow: 'f0c674',
+  green: '98c379',
+  cyan: '56b6c2',
+  blue: '61afef',
+  magenta: 'c678dd',
+  darkRed: 'e06c75',
+}
---
+++ configs/schemes/seventy.js
@@ -0,0 +1,10 @@
+const baseColors = require('../colors/soft')
+
+module.exports = {
+  name: 'Seventy',
+
+  baseDark: '282c34',
+  baseLight: 'eff1f5',
+
+  baseColors
+}
