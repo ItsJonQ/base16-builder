eda85e41b62ee409503f5cff8cd20dd524df0beb
ItsJonQ <itsjonq@gmail.com>
2018-03-15 09:37:48 -0400

    Update make script

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ cd ~/base16-builder
 cp -a ./output/shell/. ~/dotfiles/base16-shell/
 chmod 755 * ~/dotfiles/base16-shell/*
 cd ~/dotfiles/
+git pull
 git add .
 git commit -m "Update base16-shell"
 git push
@@ -14,6 +15,7 @@ cd ~/base16-builder
 # Copy to vim
 cp -a ./output/vim/. ~/base16-vim/colors
 cd ~/base16-vim
+git pull
 git add .
 git commit -m "Update base16-vim"
 git push
