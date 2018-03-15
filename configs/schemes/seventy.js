00fdc85ea1a882f897a117ca3430933abdaa54fa
ItsJonQ <itsjonq@gmail.com>
2018-03-15 11:23:01 -0400

    Update seventy

--- configs/schemes/seventy.js
+++ configs/schemes/seventy.js
@@ -5,6 +5,7 @@ const hexColor = require('../utilities/withTemplate').hexColor
 const darken = (color, value = 0.08) => hexColor(withColor(color).darken(value))
 const baseDark = '282c34'
 const baseLight = 'eff1f5'
+const orange = 'f0b260'
 
 module.exports = {
   name: 'Seventy',
@@ -13,11 +14,12 @@ module.exports = {
   baseLight,
 
   baseColors: Object.assign({}, baseColors, {
+    orange,
     yellow: darken(baseLight, 0.095)
   }),
 
   baseLightColors: Object.assign({}, baseColors, {
-    orange: darken(baseColors.orange),
+    orange: darken(orange),
     yellow: baseDark,
   })
 }
