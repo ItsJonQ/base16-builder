e94210a6d7e6c0f34b81713da80d4399da48703f
ItsJonQ <itsjonq@gmail.com>
2018-03-17 18:57:40 -0400

    Update vim

---
+++ configs/schemes/pristine.js
@@ -0,0 +1,21 @@
+const amber = 'af7847'
+const purple = 'ae8ee5'
+const gold = 'fdc391'
+
+module.exports = {
+  name: 'Pristine',
+
+  baseDark: '2c2634',
+  baseLight: 'f1eafe',
+
+  baseColors: {
+    red: amber,
+    orange: amber,
+    yellow: gold,
+    green: purple,
+    cyan: amber,
+    blue: purple,
+    magenta: amber,
+    darkRed: amber,
+  }
+}
