ea5b725c5154a669e2598bc58e733aa10f5fef32
ItsJonQ <itsjonq@gmail.com>
2018-03-24 14:23:45 -0400

    Add Pale

---
+++ configs/colors/pale.js
@@ -0,0 +1,18 @@
+const fireRed = 'd3423e'
+const gold = 'ffc98b'
+const green = '8dc891'
+const indigo = '7986e7'
+const orange = 'f78c6c'
+const paleGreen = 'c3e88d'
+const pink = 'ff5a90'
+
+module.exports = {
+  red: pink,
+  orange: orange,
+  yellow: gold,
+  green,
+  cyan: indigo,
+  blue: '82b1ff',
+  magenta: 'c792ea',
+  darkRed: pink,
+}
---
+++ configs/schemes/palenight.js
@@ -0,0 +1,10 @@
+const baseColors = require('../colors/pale')
+
+module.exports = {
+  name: 'Palenight',
+
+  baseDark: '292D3E',
+  baseLight: 'eff1f5',
+
+  baseColors
+}
