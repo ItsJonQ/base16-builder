2848415220f01412bcafb229c71555aeb8b4244f
ItsJonQ <itsjonq@gmail.com>
2018-04-11 16:32:17 -0400

    Update dracula

---
+++ configs/schemes/dracula.js
@@ -0,0 +1,21 @@
+const baseColors = {
+  red: 'ff79c6',
+  orange: 'ffB86C',
+  yellow: 'F1FA8C',
+  green: '50FA7B',
+  cyan: '8BE9FD',
+  blue: 'BD93F9',
+  magenta: 'FF79C6',
+  darkRed: 'ff79c6'
+}
+
+module.exports = {
+  name: 'Dracula',
+
+  baseDark: '282a36',
+  baseLight: 'f7f7fb',
+
+  baseColors
+}
+
+module.exports.baseColors = baseColors
