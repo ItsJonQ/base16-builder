59aa445b25c7dd37ece259005c106f797b3688f3
ItsJonQ <itsjonq@gmail.com>
2018-04-12 16:41:15 -0400

    Update One

--- configs/schemes/one.js
+++ configs/schemes/one.js
@@ -6,5 +6,17 @@ module.exports = {
   baseDark: '282c34',
   baseLight: 'eff1f5',
 
+  basePaletteColors: {
+    base00: '282C34',
+    base01: '2B3038',
+    base02: '404653',
+    base03: '5A6375',
+
+    base04: 'a7adba',
+    base05: 'c0c5ce',
+    base06: 'dfe1e8',
+    base07: 'eff1f5'
+  },
+
   baseColors
 }
