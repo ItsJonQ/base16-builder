86c7d74aaefc313192f98a98c9777b8ee2a97771
ItsJonQ <itsjonq@gmail.com>
2018-04-22 14:17:30 -0400

    Add slate

---
+++ configs/schemes/slate.js
@@ -0,0 +1,22 @@
+const baseLight = 'eeeeee'
+
+module.exports = {
+  name: 'Slate',
+  monochrome: true,
+  cursor: '50E3C2',
+  cursorText: '000000',
+
+  baseDark: '292929',
+  baseLight,
+
+  baseColors: {
+    red: baseLight,
+    orange: baseLight,
+    yellow: baseLight,
+    green: baseLight,
+    cyan: baseLight,
+    blue: baseLight,
+    magenta: baseLight,
+    darkRed: baseLight,
+  },
+}
---
+++ configs/schemes/yesterday.js
@@ -0,0 +1,23 @@
+const baseColors = require('../colors/one')
+
+module.exports = {
+  name: 'Yesterday',
+
+  baseDark: '1d1f21',
+  baseLight: 'eff1f5',
+
+  basePaletteColors: {
+    base00: '1d1f21',
+    base01: '222426',
+    base02: '313335',
+    base03: '454749',
+
+
+    base04: 'c7c9cd',
+    base05: 'dbdde1',
+    base06: 'ebecf0',
+    base07: 'eff1f5',
+  },
+
+  baseColors
+}
