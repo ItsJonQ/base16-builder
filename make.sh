31e7f08cc9c21e26f56c536505e7ff9d680c3b05
ItsJonQ <itsjonq@gmail.com>
2018-04-27 17:34:01 -0400

    Update PRPL

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ cd ~/base16-builder
 # Copy to dotfiles
 cd ~/dotfiles/
 git pull
-cp -a ./output/shell/. ~/dotfiles/base16-shell/
+cp -a ~/base16-builder/output/shell/. ~/dotfiles/base16-shell/
 chmod 755 * ~/dotfiles/base16-shell/*
 git add .
 git commit -m "Update base16-shell"
@@ -15,7 +15,7 @@ cd ~/base16-builder
 # Copy to vim
 cd ~/base16-vim
 git pull
-cp -a ./output/vim/. ~/base16-vim/colors
+cp -a ~/base16-builder/output/vim/. ~/base16-vim/colors
 git add .
 git commit -m "Update base16-vim"
 git push
