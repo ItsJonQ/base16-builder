882c3acf9cf8742db802d8aa095ee68ea9192c99
ItsJonQ <itsjonq@gmail.com>
2018-04-28 08:14:54 -0400

    Add lunar

---
+++ configs/schemes/lunar.js
@@ -0,0 +1,31 @@
+const {
+  baseColors,
+  baseLightColors
+} = require('../colors/subtle')
+
+const baseDark = '002b36'
+const baseLight = 'fdf6e3'
+
+module.exports = {
+  name: 'Lunar',
+
+  vimText: baseLight,
+  vimLightText: baseDark,
+
+  baseDark: '073642',
+  baseLight,
+
+  basePaletteColors: {
+    base00: '073642',
+    base01: '083B48',
+    base02: '0B566A',
+    base03: '107995',
+    base04: 'c8c7bb',
+    base05: 'eee8d6',
+    base06: 'f8f0dd',
+    base07: 'fdf6e3'
+  },
+
+  baseColors,
+  baseLightColors
+}
