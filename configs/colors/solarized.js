832d07b1c67b3217cc12798836682171fe47e5be
ItsJonQ <itsjonq@gmail.com>
2018-04-28 07:46:48 -0400

    Create solar

---
+++ configs/colors/solarized.js
@@ -0,0 +1,14 @@
+const baseColors = {
+  red: 'dc322f',
+  orange: 'cb4b16',
+  yellow: 'b58900',
+  green: '859900',
+  cyan: '2aa198',
+  blue: '268bd2',
+  magenta: '6c71c4',
+  darkRed: 'd33682'
+}
+
+module.exports = {
+  baseColors
+}
---
+++ configs/schemes/solar.js
@@ -0,0 +1,20 @@
+const {
+  baseColors,
+  baseLightColors
+} = require('../colors/subtle')
+
+const baseDark = '002b36'
+const baseLight = 'fdf6e3'
+
+module.exports = {
+  name: 'Solar',
+
+  vimText: baseLight,
+  vimLightText: baseDark,
+
+  baseDark,
+  baseLight,
+
+  baseColors,
+  baseLightColors
+}
