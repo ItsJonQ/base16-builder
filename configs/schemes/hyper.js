89f4c949ed7762733d5a4ef6626c62be4dfcc686
ItsJonQ <itsjonq@gmail.com>
2018-08-13 09:57:09 -0400

    Update hyper and add hype

---
+++ configs/schemes/hyper.js
@@ -0,0 +1,27 @@
+const baseColors = require('../colors/one')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'Hyper',
+
+  cursor: 'ff0080',
+  cursorText: 'ffffff',
+
+  baseDark: '000000',
+  baseLight: 'ffffff',
+
+  basePaletteColors: {
+    base00: '000000',
+    base01: '0c0c0c',
+    base02: '2c2c2c',
+    base03: '4c4c4c',
+
+    base04: 'bbbbbb',
+    base05: 'ececec',
+    base06: 'f5f5f5',
+    base07: 'ffffff',
+  },
+
+  baseColors,
+  baseLightColors
+}
