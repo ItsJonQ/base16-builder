078cb2e3840acaa01d8d21d0dfbb5f429f251b56
ItsJonQ <itsjonq@gmail.com>
2018-08-14 09:43:29 -0400

    Add paleblack

---
+++ configs/schemes/paleblack.js
@@ -0,0 +1,25 @@
+const baseColors = require('../colors/pale')
+
+module.exports = {
+  name: 'Hype',
+
+  cursor: 'ff0080',
+  cursorText: 'ffffff',
+
+  baseDark: '000000',
+  baseLight: 'ffffff',
+
+  basePaletteColors: {
+    base00: '000000',
+    base01: '0c0c0c',
+    base02: '2c2c2c',
+    base03: '4c4c4c',
+
+    base04: 'bbbbbb',
+    base05: 'ececec',
+    base06: 'f5f5f5',
+    base07: 'ffffff',
+  },
+
+  baseColors,
+}
