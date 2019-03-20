7a9f05bd8e0251aed03744ad4b5acb76b10bdb19
Jon Q <hello@jonquach.com>
2019-03-19 21:09:26 -0400

    Fix G2 and G3

--- configs/schemes/g2.js
+++ configs/schemes/g2.js
@@ -4,6 +4,6 @@ const mainColor = '31c794';
 const baseDark = '11151c';
 const baseLight = '9bd1cf';
 
-module.exports = Object.assign(makeColor(mainColor), {
+module.exports = Object.assign(makeColor(mainColor, baseDark, baseLight), {
   name: 'G2',
 });
