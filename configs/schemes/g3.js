bfc0cdeff11d1ffc02cab5fd75431395c51ce3d7
Jon Q <hello@jonquach.com>
2019-03-19 21:08:21 -0400

    Add G2 and G3

---
+++ configs/schemes/g3.js
@@ -0,0 +1,9 @@
+const makeColor = require('../colors/overwatch');
+
+const mainColor = 'd06935';
+const baseDark = '11151c';
+const baseLight = '9bd1cf';
+
+module.exports = Object.assign(makeColor(mainColor), {
+  name: 'G3',
+});
