fce07b713d7482770d8dfd3ff1a53e6d3812ae0b
Jon Q <hello@jonquach.com>
2019-03-23 19:24:59 -0400

    Add Go + Update Ayudark

---
+++ configs/schemes/ayudark.js
@@ -0,0 +1,12 @@
+const baseColors = require('../colors/ayu')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'Ayu Dark',
+
+  baseDark: '0b0e13',
+  baseLight: 'eff1f5',
+
+  baseColors,
+  baseLightColors,
+}
---
+++ configs/schemes/go.js
@@ -0,0 +1,12 @@
+const baseColors = require('../colors/one')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'Go',
+
+  baseDark: '323638',
+  baseLight: 'eff1f5',
+
+  baseColors,
+  baseLightColors,
+}
