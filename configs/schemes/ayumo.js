db5bd4385c8e0f13671f83dd24ed6cccdf203fa8
Jon Q <hello@jonquach.com>
2019-03-23 12:33:14 -0400

    Add ayumo

---
+++ configs/schemes/ayumo.js
@@ -0,0 +1,24 @@
+const baseColors = require('../colors/ayu')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'Ayu Mo',
+
+  baseDark: '1b1b1b',
+  baseLight: 'ffffff',
+
+  basePaletteColors: {
+    base00: '1d1d1e',
+    base01: '282828',
+    base02: '3c3c3c',
+    base03: '4c4c4c',
+
+    base04: 'cfcfcf',
+    base05: 'ECECEC',
+    base06: 'F7F7F7',
+    base07: 'F7F7F7',
+  },
+
+  baseColors,
+  baseLightColors,
+}
