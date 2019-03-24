75af48e0659a406d2dc93838e5bd8c8f695945e5
Jon Q <hello@jonquach.com>
2019-03-24 17:35:39 -0400

    Add Ayubuntu

---
+++ configs/schemes/ayubuntu.js
@@ -0,0 +1,24 @@
+const baseColors = require('../colors/ayu')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'Ayu Buntu',
+
+  baseDark: '411934',
+  baseLight: 'FBF3F7',
+
+  basePaletteColors: {
+    base00: '411934',
+    base01: '4B233E',
+    base02: '552D48',
+    base03: '9E7D96',
+
+    base04: 'BB94A9',
+    base05: 'EEE9ED',
+    base06: 'FBF3F7',
+    base07: 'FBF3F7',
+  },
+
+  baseColors,
+  baseLightColors,
+}
