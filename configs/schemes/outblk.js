e887c84b38cf702f56046bfa20a3f1658526c93b
Jon Q <hello@jonquach.com>
2019-04-19 12:07:48 -0400

    Add outrun

---
+++ configs/schemes/outblk.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'Outblk',
+
+  baseDark: '000000',
+  baseLight: 'ffffff',
+
+  basePaletteColors: {
+    base00: '000000',
+    base01: '0c0c0c',
+    base02: '2c2c2c',
+    base03: '4c4c4c',
+
+    base04: 'bbbbbb',
+    base05: 'ececec',
+    base06: 'f5f5f5',
+    base07: 'ffffff',
+  },
+
+  baseColors: {
+    red: 'f706cf',
+    orange: 'ff6c11',
+    yellow: 'f9c80e',
+    green: '2de2e6',
+    cyan: '2de2e6',
+    blue: '1496ff',
+    magenta: 'f6019d',
+    darkRed: 'd40078',
+  },
+};
---
+++ configs/schemes/outrun.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'Outrun',
+
+  baseDark: '0d0221',
+  baseLight: 'eff1f5',
+
+  basePaletteColors: {
+    base00: '0D0221',
+    base01: '160c2d',
+    base02: '281c39',
+    base03: '372a3f',
+
+    base04: 'B3BDD0',
+    base05: 'D9DDE7',
+    base06: 'E6E9EF',
+    base07: 'EFF1F5',
+  },
+
+  baseColors: {
+    red: 'f706cf',
+    orange: 'ff6c11',
+    yellow: 'f9c80e',
+    green: '2de2e6',
+    cyan: '2de2e6',
+    blue: '1496ff',
+    magenta: 'f6019d',
+    darkRed: 'd40078',
+  },
+};
