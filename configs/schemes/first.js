b0820bde0ecd732c72ca21061c45a3b5d1a4424c
Jon Q <hello@jonquach.com>
2019-04-22 12:08:30 -0400

    Add first colour scheme

---
+++ configs/schemes/first.js
@@ -0,0 +1,24 @@
+const baseColors = require('../colors/one')
+const baseLightColors = require('../colors/oneLight')
+
+module.exports = {
+  name: 'First',
+
+  baseDark: '181818',
+  baseLight: 'f8f8f8',
+
+  basePaletteColors: {
+    base00: '181818',
+    base01: '242424',
+    base02: '383838',
+    base03: '484848',
+
+    base04: 'b8b8b8',
+    base05: 'd8d8d8',
+    base06: 'ececec',
+    base07: 'f8f8f8',
+  },
+
+  baseColors,
+  baseLightColors
+}
