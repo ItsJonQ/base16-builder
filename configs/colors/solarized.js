026d6f846c80dc7d185f5b281ad33a9d6fed3682
Jon Q <hello@jonquach.com>
2019-04-23 15:51:25 -0400

    Update solarized colors

--- configs/colors/solarized.js
+++ configs/colors/solarized.js
@@ -6,9 +6,22 @@ const baseColors = {
   cyan: '2aa198',
   blue: '268bd2',
   magenta: '6c71c4',
-  darkRed: 'd33682'
+  darkRed: 'd33682',
+}
+
+const basePaletteColors = {
+  base00: '002b36',
+  base01: '0a3540',
+  base02: '1e4954',
+  base03: '325d68',
+
+  base04: 'cac4b3',
+  base05: 'ded8c7',
+  base06: 'f2ecdb',
+  base07: 'fcf6e5',
 }
 
 module.exports = {
-  baseColors
+  baseColors,
+  basePaletteColors,
 }
---
+++ configs/schemes/solarized.js
@@ -0,0 +1,17 @@
+const { basePaletteColors } = require('../colors/solarized')
+const { baseColors, baseLightColors } = require('../colors/subtle')
+
+const baseDark = '002b36'
+const baseLight = 'fdf6e3'
+
+module.exports = {
+  name: 'Solarized',
+
+  baseDark,
+  baseLight,
+
+  basePaletteColors,
+
+  baseColors,
+  baseLightColors
+}
