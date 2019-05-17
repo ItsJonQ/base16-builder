3699511f868d689f438d51f947713df99df4e229
Jon Q <hello@jonquach.com>
2019-05-17 10:03:51 -0400

    Update nightowl

--- configs/schemes/nightowl.js
+++ configs/schemes/nightowl.js
@@ -6,6 +6,17 @@ module.exports = {
   baseDark: '011627',
   baseLight: 'eff1f5',
 
-  baseColors
-}
+  basePaletteColors: {
+    base00: '011627',
+    base01: '02213b',
+    base02: '02355e',
+    base03: '034377',
 
+    base04: 'b3bdd0',
+    base05: 'd9dde7',
+    base06: 'e6e9ef',
+    base07: 'eff1f5',
+  },
+
+  baseColors,
+}
