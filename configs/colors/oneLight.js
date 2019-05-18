3a7ea3d7b24d19bc027b57419ad4a5f1bde47e7d
Jon Q <hello@jonquach.com>
2019-05-18 11:29:11 -0400

    Add Basic theme

--- configs/colors/oneLight.js
+++ configs/colors/oneLight.js
@@ -1,15 +1,15 @@
-const baseColors = require('./one')
-const darken = require('../utilities/color').darken
+const baseColors = require('./one');
+const darken = require('../utilities/color').darken;
 
 const baseLightColors = {
-  red: darken(baseColors.red),
-  orange: darken(baseColors.orange),
-  yellow: darken(baseColors.yellow),
-  green: darken(baseColors.green),
-  cyan: darken(baseColors.cyan),
-  blue: darken(baseColors.blue),
-  magenta: darken(baseColors.magenta),
-  darkRed: darken(baseColors.darkRed),
-}
+  red: darken(baseColors.red, 0.2),
+  orange: darken(baseColors.orange, 0.2),
+  yellow: darken(baseColors.yellow, 0.2),
+  green: darken(baseColors.green, 0.2),
+  cyan: darken(baseColors.cyan, 0.2),
+  blue: darken(baseColors.blue, 0.2),
+  magenta: darken(baseColors.magenta, 0.2),
+  darkRed: darken(baseColors.darkRed, 0.2),
+};
 
-module.exports = baseLightColors
+module.exports = baseLightColors;
---
+++ configs/schemes/basic.js
@@ -0,0 +1,24 @@
+const baseColors = require('../colors/one');
+const baseLightColors = require('../colors/oneLight');
+
+module.exports = {
+  name: 'Basic',
+
+  baseDark: '181818',
+  baseLight: 'f8f8f8',
+
+  basePaletteColors: {
+    base00: '181818',
+    base01: '1d1d1d',
+    base02: '282828',
+    base03: '484848',
+
+    base04: 'b8b8b8',
+    base05: 'e8e8e8',
+    base06: 'f2f2f2',
+    base07: 'f8f8f8',
+  },
+
+  baseColors,
+  baseLightColors,
+};
