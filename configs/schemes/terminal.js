828066cffc7cf575d56dbf859b6a219151fcfce9
Jon Q <hello@jonquach.com>
2020-05-21 10:22:04 -0400

    Update terminal

--- configs/schemes/terminal.js
+++ configs/schemes/terminal.js
@@ -1,52 +1,53 @@
-const darken = require('../utilities/color').darken
+const darken = require('../utilities/color').darken;
 
-const red = 'eb6f6b'
-const magenta = 'fd70fb'
-const orange = 'eb8500'
+const red = 'ff0032';
+const green = '00FF68';
+const magenta = 'fd70fb';
+const orange = 'eb8500';
 // const cyan = '318495'
-const cyan = '67fdff'
-const cyanLight = '606aa1'
-const yellow = 'fffe72'
-const darkYellow = 'c7c626'
+const cyan = '67fdff';
+const cyanLight = '606aa1';
+const yellow = 'fffe72';
+const darkYellow = 'c7c626';
 
 // const baseText = 'f3f3f3'
 // const baseLightText = '0c0c0c'
-const baseText = (baseLightText = cyan)
+const baseText = (baseLightText = cyan);
 
 const baseColors = {
-  red,
-  orange: magenta,
-  yellow,
-  green: '1ac428',
-  cyan,
-  blue: '32b4ff',
-  magenta,
-  darkRed: red,
-}
+	red,
+	orange: magenta,
+	yellow,
+	green,
+	cyan,
+	blue: '32b4ff',
+	magenta,
+	darkRed: red,
+};
 
 module.exports = {
-  name: 'Terminal',
+	name: 'Terminal',
 
-  baseDark: '000000',
-  baseLight: 'ffffff',
+	baseDark: '000000',
+	baseLight: 'ffffff',
 
-  basePaletteColors: {
-    base00: '000000',
-    base01: '0c0c0c',
-    base02: '2c2c2c',
-    base03: '4c4c4c',
+	basePaletteColors: {
+		base00: '000000',
+		base01: '0c0c0c',
+		base02: '2c2c2c',
+		base03: '4c4c4c',
 
-    base04: 'bbbbbb',
-    base05: 'ececec',
-    base06: 'f5f5f5',
-    base07: 'ffffff',
-  },
+		base04: 'bbbbbb',
+		base05: 'ececec',
+		base06: 'f5f5f5',
+		base07: 'ffffff',
+	},
 
-  baseColors,
+	baseColors,
 
-  baseLightColors: Object.assign({}, baseColors, {
-    yellow: darkYellow,
-    cyan: cyanLight,
-    blue: '143ccd',
-  }),
-}
+	baseLightColors: Object.assign({}, baseColors, {
+		yellow: darkYellow,
+		cyan: cyanLight,
+		blue: '143ccd',
+	}),
+};
